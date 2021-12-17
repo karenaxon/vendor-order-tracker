c7ab2279761a435ff64ef7f525abdd86053c8c1a
Kaxon <karenaxon@users.noreply.github.com>
2021-12-17 10:31:00 -0800

    Green Order constructor test.

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("Monday", "Every Monday morning", "435.78", "04/25/2021");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+  }
+    
+}
